7ae7b8e873266a3b2c88ac24325599444b25e444
Suraj <hingadesuraj@gmail.com>
2024-02-27 18:50:35 +0530

    init

--- tailwind_first_project/src/components/AboutGrid.jsx
+++ tailwind_first_project/src/components/AboutGrid.jsx
@@ -4,7 +4,7 @@ const AboutGrid = ({icon,text,label,heading}) => {
   return (
     <div className='flex justify-center items-center gap-55 flex-col bg-white rounded-lg m-2 p-3 lg:p-7' >
         <img src={icon} alt="icon" width={65} height={65} className=' bg-green-400 p-3 rounded-full hover:bg-green-500 cursor-pointer' />
-        <h1 className=' text-2xl lg:text-3xl text-center  text-green-600 font-semibold ' >{heading}</h1>
+        <h1 className=' text-2xl px-2 lg:text-3xl text-center  text-green-600 font-semibold ' >{heading}</h1>
         <p className=' text-[18px] text-slate-700  text-center ' >{text}</p>
         <button className='bg-green-800 text-white px-4 lg:px-8 py-2 rounded-md text-[18px] hover:bg-black hover:text-white cursor-pointer ' >View More</button>
     </div>
