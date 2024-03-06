827e5da3716909fec9d16a2af7dd7637e97fbdea
Suraj <hingadesuraj@gmail.com>
2024-03-06 14:03:31 +0530

    fecture section done

--- tailwind_first_app/src/components/Features.jsx
+++ tailwind_first_app/src/components/Features.jsx
@@ -7,7 +7,7 @@ const Features = () => {
             <img src="https://images.unsplash.com/photo-1474511320723-9a56873867b5?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW5pbWFsfGVufDB8fDB8fHwwhttps://cdn2.iconfinder.com/data/icons/icontober/64/Inkcontober_Trail-256.png" className="w-full mb-4 rounded border-solid border-2 border-yellow-300" alt="image 1" />
             <p>Image 1</p>
         </div>  
-        <div className='mr-4 text-center'>
+        <div className='mr-4 mt-12 text-center'>
             <img src="https://images.unsplash.com/photo-1474511320723-9a56873867b5?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW5pbWFsfGVufDB8fDB8fHwwhttps://cdn2.iconfinder.com/data/icons/icontober/64/Inkcontober_Trail-256.png" className="w-full mb-4 rounded border-solid border-2 border-yellow-300" alt="image 2" />
             <p>Image 2</p>
         </div>
@@ -15,7 +15,7 @@ const Features = () => {
             <img src="https://images.unsplash.com/photo-1474511320723-9a56873867b5?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW5pbWFsfGVufDB8fDB8fHwwhttps://cdn2.iconfinder.com/data/icons/icontober/64/Inkcontober_Trail-256.png" className="w-full mb-4 rounded border-solid border-2 border-yellow-300" alt="imgae 3" />
             <p>Image 3</p>
         </div>
-        <div className='mr-4 text-center'>
+        <div className='mr-4 mt-12 text-center'>
             <img src="https://images.unsplash.com/photo-1474511320723-9a56873867b5?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW5pbWFsfGVufDB8fDB8fHwwhttps://cdn2.iconfinder.com/data/icons/icontober/64/Inkcontober_Trail-256.png" className="w-full   mb-4 rounded border-solid border-2 border-yellow-300 " alt="imgae 4" />
             <p>Imgae 4</p>
         </div>
