487655d0e5c41137eabc556973fd41ba7e618cfd
Suraj <hingadesuraj@gmail.com>
2024-03-06 13:04:43 +0530

    created hover effect in tailwind css

--- tailwind_first_app/src/App.jsx
+++ tailwind_first_app/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-    <div className="bg-indigo-300" >
+    <div>
      <Header />
     </div>
     </>
--- tailwind_first_app/src/components/Header.jsx
+++ tailwind_first_app/src/components/Header.jsx
@@ -3,21 +3,22 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div>
+      <div className="bg-indigo-300 px-10 py-4 flex justify-between items-center gap-4 ">
         {/* left side */}
-        {/* logo : iconfinder.com  */}
-        <img
-          src="https://cdn2.iconfinder.com/data/icons/icontober/64/Inkcontober_Trail-256.png"
-          alt="logo_Mountina"
-          width={50}
-        />
-        <a href="">Home</a>
-        <a href="">About</a>
-
+        <div className="flex justify-between items-center gap-6" >
+          {/* logo : iconfinder.com  */}
+          <img
+            src="https://cdn2.iconfinder.com/data/icons/icontober/64/Inkcontober_Trail-256.png"
+            alt="logo_Mountina"
+            width={50}
+          />
+          <a href="" className=" inline-block p-2 text-gray-700 hover:border-1 hover:text-black hover:bg-indigo-200 rounded-md ">Home</a>
+          <a href="" className=" inline-block p-2 text-gray-700 hover:border-1 hover:text-black hover:bg-indigo-200 rounded-md ">About</a>
+        </div>
         {/* right side */}
-        <div>
-            <a href="">Login</a>
-            <a href="">Signup</a>
+        <div className="flex justify-between gap-6" >
+          <a href="">Login</a>
+          <a href="">Signup</a>  
         </div>
       </div>
     </>
