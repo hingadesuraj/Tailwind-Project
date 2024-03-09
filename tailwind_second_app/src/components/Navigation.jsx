55829a090b5a61050e9ebd198c04c6d63799ea35
Suraj <hingadesuraj@gmail.com>
2024-03-09 19:35:12 +0530

    Start second Project using tailwind css

---
+++ tailwind_second_app/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+        {/* left */}
+        <div>
+
+        </div>
+        {/* middle */}
+        <div>
+
+        </div>
+        {/* right */}
+        
+    </div>
+  )
+}
+
+export default Navigation
