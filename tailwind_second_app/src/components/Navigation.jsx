0d7aa4cb6469194e860ffbab738b7323a4f11a10
Suraj <hingadesuraj@gmail.com>
2024-03-09 20:49:13 +0530

    Navigation bar complete

--- tailwind_second_app/src/components/Navigation.jsx
+++ tailwind_second_app/src/components/Navigation.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import Button from './miniComponent/Button'
+import React from "react";
+import Button from "./miniComponent/Button";
 
 const Navigation = () => {
   return (
-    <div className='flex justify-between items-center mx-32 my-4 ' >
-        {/* left */}
-        <div className='flex justify-center items-center gap-3' >
-            <img src="./images/logo.svg" alt="Logo" />
-            <h1 className=' font-semibold' >Landwind</h1>
+    <div className="flex justify-between items-center mx-[155px] my-3 ">
+      {/* left */}
+      <div className="flex justify-center items-center gap-3">
+        <img className="w-10" src="./images/logo.svg" alt="Logo" />
+        <h1 className=" text-xl font-semibold">Landwind</h1>
+      </div>
+      {/* middle */}
+      <div>
+        <div className="flex justify-center items-center list-none gap-8 font-semibold ">
+          <li>Home</li>
+          <li>Company</li>
+          <li>Marketplace</li>
+          <li>Features</li>
+          <li>Team</li>
+          <li>Contace</li>
         </div>
-        {/* middle */}
+      </div>
+      {/* right */}
+      <div className="flex justify-center items-center gap-3">
+        <div className="flex justify-center items-center  px-3">
+          <h1 className=" flex border-y-2 border-l-2 pr-2 bg-gray-300 rounded-sm" >⭐Star</h1>
+          <h1 className="px-2 rounded-sm border-y-2 border-r-2 ">680</h1>
+        </div>
         <div>
-          <div className='flex justify-center items-center list-none gap-8 font-semibold' >
-            <li>Home</li>
-            <li>Company</li>
-            <li>Marketplace</li>
-            <li>Features</li>
-            <li>Team</li>
-            <li>Contace</li>
-          </div>
+          <Button title="Download" />
         </div>
-        {/* right */}
-        <div className='flex justify-center items-center gap-3'>
-            <div className='flex justify-center items-center gap-1' >
-               <h1>Start</h1>
-               <h1>680</h1>
-            </div>
-            <div>
-                <Button title="Download" />
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
--- tailwind_second_app/src/components/miniComponent/Button.jsx
+++ tailwind_second_app/src/components/miniComponent/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({title}) => {
   return (
     <div>
-        <button className='  bg-[#6B2BD7] px-[8px] py-[8px] rounded-md text-white font-medium ' >{title}</button>
+        <button className='  bg-[#6B2BD7] px-[12px] py-[6px] rounded-md text-white font-medium ' >{title}</button>
     </div>
   )
 }
